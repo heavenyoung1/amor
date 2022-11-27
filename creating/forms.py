6ddb235c76bff4be04defc6a053b432ba7753fb6
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-11-27 19:31:40 +0300

    Update

--- creating/forms.py
+++ creating/forms.py
@@ -1,11 +1,8 @@
 from django import forms
 from creating.models import User
+from django.core.exceptions import ValidationError
 
 class UserInput(forms.ModelForm):
     class Meta:
         model = User
         fields = "__all__" #добавить поля формы явно
-
-    def clean_name(self):
-        name = self.cleaned_data['name']
-        if len(name) < $
--- creating/models.py
+++ creating/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class User(models.Model):
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    SEX_CHOICES = (
+        ('man', 'Женщина'),
+        ('woman', 'Мужчина'),
+    )
+
+    sex = models.CharField(max_length=6, choices=SEX_CHOICES, default='woman')
+
+    city = models.CharField(max_length=100)
+    old = models.CharField(max_length=100)
+
+
+
+
+
 
