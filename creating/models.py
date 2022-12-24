10fdb0c0b0b905fb5c160a1cb8f45db8a57e0841
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-12-24 17:07:39 +0300

    Update

--- creating/models.py
+++ creating/models.py
@@ -1,19 +1,35 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+User = get_user_model()
 
-    SEX_CHOICES = (
-        ('man', 'Женщина'),
-        ('woman', 'Мужчина'),
-    )
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    #profileimg = pass
+    location = models.CharField(max_length=100, blank=True)
 
-    sex = models.CharField(max_length=6, choices=SEX_CHOICES, default='woman')
+    def __str__(self):
+        return self.user.username
 
-    city = models.CharField(max_length=100)
-    old = models.CharField(max_length=100)
+
+
+
+# class User(models.Model):
+#     name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
+#
+#     SEX_CHOICES = (
+#         ('man', 'Женщина'),
+#         ('woman', 'Мужчина'),
+#     )
+#
+#     sex = models.CharField(max_length=6, choices=SEX_CHOICES, default='woman')
+#
+#     city = models.CharField(max_length=100)
+#     old = models.CharField(max_length=100)
 
 
 
